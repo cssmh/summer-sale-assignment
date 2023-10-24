8d88923e593e1e578304171ec7294c8c84219322
momen210 <touristmomen@gmail.com>
2023-10-24 23:45:30 +0600

    append child added

--- index.js
+++ index.js
@@ -19,14 +19,39 @@ function Momen(btnId, priceId) {
     getTotalFirstFloat.innerText = fixer;
   });
 }
-Momen('kitchen-one', 'kitchen-one-price')
-Momen('kitchen-two', 'kitchen-two-price')
-Momen('cooker', 'cooker-price')
-Momen('cap', 'cap-price')
-Momen('jersey', 'jersey-price')
-Momen('cates', 'cates-price')
-Momen('relax-chair', 'relax-chair-price')
-Momen('children-play', 'children-play-price')
-Momen('sofa', 'sofa-price')
+Momen("kitchen-one", "kitchen-one-price");
+Momen("kitchen-two", "kitchen-two-price");
+Momen("cooker", "cooker-price");
+Momen("cap", "cap-price");
+Momen("jersey", "jersey-price");
+Momen("cates", "cates-price");
+Momen("relax-chair", "relax-chair-price");
+Momen("children-play", "children-play-price");
+Momen("sofa", "sofa-price");
 // First total amount added done
 
+// document.getElementById("kitchen-one").addEventListener("click", function () {
+//   const li = document.createElement("li");
+//   const getTitle = document.getElementById("kitchen-one-title").innerText;
+//   li.innerText = getTitle;
+//   const appendId = document.getElementById("append-id");
+//   appendId.appendChild(li);
+// });
+function Momin(idBtn, titleId) {
+  document.getElementById(idBtn).addEventListener("click", function () {
+    const li = document.createElement("li");
+    const getTitle = document.getElementById(titleId).innerText;
+    li.innerText = getTitle;
+    const appendId = document.getElementById("append-id");
+    appendId.appendChild(li);
+  });
+}
+Momin("kitchen-one", "kitchen-one-title");
+Momin("kitchen-two", "kitchen-two-title");
+Momin("cooker", "cooker-title");
+Momin("cap", "cap-title");
+Momin("jersey", "jersey-title");
+Momin("cates", "cates-title");
+Momin("relax-chair", "relax-chair-title");
+Momin("children-play", "children-play-title");
+Momin("sofa", "sofa-title");
